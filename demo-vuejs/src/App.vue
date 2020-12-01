3281ba4bf14d68ba4396395d4b70e61615df4c3e
Austin Smith <austinfosec@gmail.com>
2020-12-01 04:29:00 -0600

    vuejs sample app update

--- demo-vuejs/src/App.vue
+++ demo-vuejs/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="This Vue.js App Was Deployed Using AWS CDK"/>
   </div>
 </template>
 
