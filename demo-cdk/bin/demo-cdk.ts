3fe500e4736db57917b19a24d9880141d0fdcf2c
Austin Smith <austinfosec@gmail.com>
2020-12-01 05:09:42 -0600

    static html for now

--- demo-cdk/bin/demo-cdk.ts
+++ demo-cdk/bin/demo-cdk.ts
@@ -4,4 +4,12 @@ import * as cdk from '@aws-cdk/core';
 import { DemoCdkStack } from '../lib/demo-cdk-stack';
 
 const app = new cdk.App();
-new DemoCdkStack(app, 'DemoCdkStack');
+
+new DemoCdkStack(app, 'DemoCdkStack', {
+
+    // add aws region to app
+    env: {
+        region: 'us-east-1',
+    },
+
+});
--- demo-cdk/lib/demo-cdk-stack.ts
+++ demo-cdk/lib/demo-cdk-stack.ts
@@ -1,9 +1,30 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
+// install cmd: 'npm i @aws-cdk/aws-s3-deployment'
 
 export class DemoCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    super(scope, id, props);
+
     // The code that defines your stack goes here
+    // create bucket
+    const myBucket = new s3.Bucket(this, "WebsiteBucketForDemo", {
+      // allow bucket to access to public internet
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: "index.html"
+    });
+
+    // create deployment resource
+    // constructs new instance of BucketDeployment class
+    const deployment = new s3Deployment.BucketDeployment(this, "deployStaticWebsite", {
+      // directory of the web asset
+      sources: [s3Deployment.Source.asset("./web-app")],
+      // (experimental) The S3 bucket to sync the contents of the zip file to.)
+      destinationBucket: myBucket
+   });
   }
 }
